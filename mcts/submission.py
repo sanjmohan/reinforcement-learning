2afa62851c280a7e35d0a51a2964f1c7fb6a829e
Sanjay Mohan <sam2mohan@gmail.com>
2020-05-24 03:25:19 -0700

    slow agent that beats negamax consistently

--- mcts/submission.py
+++ mcts/submission.py
@@ -1,22 +1,64 @@
 # https://www.kaggle.com/c/connectx/overview
 # Everything encapsulated in one method because of submission format
 
-import numpy as np
-
 def agent(obs, config):
+    import numpy as np
 
     def check_outcome(board, col, col_rows):
-        # check the spot at (col_rows[col] + 1, col) for winning move, or if col_rows < 0 then draw
-        # return None if not game over
-        raise NotImplementedError()
-
+        # 1 = win (for mark at spot (col_rows[col], col)), 0 = draw, None = not finished
+        # Check if board filled:
+        for c in range(config.columns):
+            if col_rows[c] >= 0:
+                break
+        else:
+            return 0  # draw
+        row = col_rows[col] + 1
+        mark = board[row, col]
+        # check column - will only be a streak in the top `inarow` spots of the column
+        if row < config.rows - config.inarow + 1:
+            for r in range(config.inarow):
+                if board[row + r, col] != mark:
+                    break
+            else:
+                return 1  # column win
+        # check row + diags
+        row_matches = 0
+        updiag_matches = 0
+        downdiag_matches = 0
+        for c in range(max(0, col-config.inarow+1), min(config.columns, col+config.inarow)):
+            offset = c - col
+            if board[row, c] == mark:
+                row_matches += 1
+                if row_matches == config.inarow:
+                    return 1  # row win
+            else:
+                row_matches = 0
+            if 0 <= row + offset < config.rows and board[row+offset, c] == mark:
+                updiag_matches += 1
+                if updiag_matches == config.inarow:
+                    return 1  # upwards diagonal win
+            else:
+                updiag_matches = 0
+            if 0 <= row - offset < config.rows and board[row-offset, c] == mark:
+                downdiag_matches += 1
+                if downdiag_matches == config.inarow:
+                    return 1  # downward diagonal win
+            else:
+                downdiag_matches = 0
+        return None
+    
+    # in place
     def play(board, col, col_rows, mark):
         row = col_rows[col]
-        board[row*config.columns + col] = mark
+        board[row, col] = mark
         col_rows[col] -= 1
-
-    def uct(nodes):
-        raise NotImplementedError()
+        
+    def uct(nodes, t, c=1):
+        try:
+            best = np.argmax([n.wins/n.plays + c*np.sqrt(2*np.log(t)/n.plays) for n in nodes])
+        except:
+            print([n.__dict__ for n in nodes])
+        return nodes[best]
 
     class MCTSNode():
         def __init__(self, col, mark):
@@ -25,24 +67,33 @@ def agent(obs, config):
             self.plays = 0
             self.wins = 0
             self.children = []
-
+            
         def play(self, board, col_rows):
             play(board, self.col, col_rows, self.mark)
-
+            
         def expand(self, board, col_rows):
             self.play(board, col_rows)
-            if len(children) == config.columns:
-                child = ucb(children)
-                res = child.expand(board, col_rows)
-            else:
-                children.append(MCTSNode(len(children)+1, 2-self.mark))
-                res = children[-1].simulate(board, col_rows)
             self.plays += 1
-            self.wins += 1 - res
-            return 1 - res
-
+            if len(self.children) == 0:
+                this_res = check_outcome(board, self.col, col_rows)
+                if this_res is not None:
+                    self.wins += this_res
+                    return this_res
+            if len(self.children) == config.columns:
+                # use upper confidence bound to choose best move out of valid moves
+                valid_moves = [ch for col,ch in enumerate(self.children) if col_rows[col] >= 0]
+                child = uct(valid_moves, self.plays)
+                child_res = child.expand(board, col_rows)
+            else:
+                self.children.append(MCTSNode(len(self.children), 3-self.mark))
+                child_res = self.children[-1].simulate(board, col_rows)
+            self.wins += 1 - child_res
+            return 1 - child_res
+            
         def simulate(self, board, col_rows):
             self.play(board, col_rows)
+            self.plays += 1
+            mark = self.mark
             res = check_outcome(board, self.col, col_rows)
             while res is None:
                 # switch player
@@ -53,20 +104,36 @@ def agent(obs, config):
                 res = check_outcome(board, col, col_rows)
                 #if col_rows[col] < 0:
                     #open_cols[col] = 0
-            return res
+            if res == 1:
+                if mark == self.mark:
+                    self.wins += 1
+                    return 1
+                return 0
+            return 0.5
 
-#     # first open row in each column (negative => column is full)
-#     col_rows = -1*np.ones(config.columns, dtype=np.int)
-#     for c in range(config.columns):
-#         for r in range(config.rows):
-#             if board[r*c + c] != 0:
-#                 break
-#             col_rows[c] = r
-#     # mask of cols that aren't full
-#     #open_cols = np.ones(config.columns, dtype=np.uint8)
-#     #for c in range(config.columns):
-#     #    if col_rows[c] < 0:
-#     #        open_cols[c] = 0
-
-    raise NotImplementedError()
-
+    board = np.copy(obs.board)
+    board = board.reshape((config.rows, config.columns))
+    col_rows = -1*np.ones(config.columns, dtype=np.int)
+    for c in range(config.columns):
+        for r in range(config.rows):
+            if board[r,c] != 0:
+                break
+            col_rows[c] = r
+    # Initialize nodes
+    nodes = [MCTSNode(c, obs.mark) for c in range(config.columns) if col_rows[c] >= 0]
+    for node in nodes:
+        tmpboard = np.copy(board)
+        tmpcol_row = np.copy(col_rows)
+        node.expand(tmpboard, tmpcol_row)
+    # Proceed by UCT
+    num_iters = 10000
+    for t in range(num_iters):
+        tmpboard = np.copy(board)
+        tmpcol_row = np.copy(col_rows)
+        node = uct(nodes, t+1)
+        node.expand(tmpboard, tmpcol_row)
+    # Choose best move
+    best = int(np.argmax([n.wins/n.plays for n in nodes]))
+    return nodes[best].col
+#     print( int(np.argmax([n.wins/n.plays for n in nodes])))
+#     return nodes
