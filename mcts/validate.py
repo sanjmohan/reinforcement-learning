b8f6be655bbc154a64da9ec541ae01e0096c177e
Sanjay Mohan <sam2mohan@gmail.com>
2020-05-26 07:17:48 -0700

    use given submission file

--- mcts/validate.py
+++ mcts/validate.py
@@ -1,9 +1,12 @@
 from kaggle_environments import evaluate, make, utils
 import sys
 
+agent_file = 'submission.py'
+if len(sys.argv) >= 2:
+    agent_file = sys.argv[1]
 
 out = sys.stdout
-submission = utils.read_file("submission.py")
+submission = utils.read_file(agent_file)
 agent = utils.get_last_callable(submission)
 sys.stdout = out
 
@@ -11,7 +14,7 @@ env = make("connectx")
 # passing timeout configuration to make doesn't construct env properly...
 # validate against 5s timeout like in kaggle leaderboard
 env.configuration.timeout = env.configuration.actTimeout = 5
-print("Validating - this may take a minute...")
+print("Validating agent from {} - this may take a minute...".format(agent_file))
 s = env.run([agent, agent])
-print("Success!" if env.state[0].status == env.state[1].status == "DONE" else "Failed..."+"\n"+s)
+print("Success!" if env.state[0].status == env.state[1].status == "DONE" else "Failed..."+"\n"+str(s))
 
